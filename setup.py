154f722f05fc57106dc8f4d7bd5fde3386c77944
Halocline <mdglissmann@gmail.com>
2020-02-22 09:40:44 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='poacher-catcher',
+    version='0.1.0',
+    description='',
+    long_description='',
+    author='',
+    author_email='',
+    url="",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'google-assistant-library>=1.0.1',
+        'google-assistant-grpc>=0.2.0',
+        'google-auth>=1.5.1',
+        'google-auth-oauthlib>=0.2.0',
+        'google-cloud-speech>=0.36.0',
+        'gpiozero',
+        'protobuf>=3.6.1',
+        'picamera',
+        'Pillow',
+        'RPi.GPIO',
+    ],
+    python_requires='>=3.5.3',
+)
