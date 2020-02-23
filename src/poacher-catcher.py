5cebf4c7a38a776f3e56fdde087315da77932016
Halocline <mdglissmann@gmail.com>
2020-02-23 15:07:54 -0700

    adding face detection trigger

--- src/poacher-catcher.py
+++ src/poacher-catcher.py
@@ -8,6 +8,10 @@ import time
 from aiy.board import Board, Led
 from aiy.leds import (Leds, Pattern, PrivacyLed, RgbLeds, Color)
 from aiy.toneplayer import TonePlayer
+from aiy.vision.inference import CameraInference
+from aiy.vision.models import face_detection
+
+from picamera import PiCamera
 
 
 def printPressDuration():
@@ -15,41 +19,52 @@ def printPressDuration():
     time.sleep(1)
 
 
+def facedetect():
+    with PiCamera() as camera:
+        # Configure camera
+        camera.resolution = (1640, 922)  # Full Frame, 16:9 (Camera v2)
+        camera.start_preview()
+
+        # Do inference on VisionBonnet
+        with CameraInference(face_detection.model()) as inference:
+            for result in inference.run():
+                if len(face_detection.get_faces(result)) >= 1:
+                    camera.capture('faces.jpg')
+                    break
+
+        # Stop preview
+        camera.stop_preview()
+
+
 def main():
     print('Press Button start. Hold Button for 5 seconds (or press Ctrl-C) to quit.')
 
     pressDuration = 0
-    pressed = False
 
     with Board() as board:
         while True:
             board.button.wait_for_press()
-            pressed = True
             pressTime = datetime.datetime.now()
             board.led.state = Led.ON
             print('ON')
+            print('Running facedetect')
+            facedetect()
 
             board.button.wait_for_release()
-            pressed = False
             releaseTime = datetime.datetime.now()
             board.led.state = Led.OFF
             print('OFF')
-
-            while pressed:
-                printPressDuration()
 
             pressDuration = releaseTime - pressTime
             print('Button pressed for ' + str(pressDuration.seconds) + ' seconds')
             if pressDuration.seconds >= 5:
                 Leds().update(Leds.rgb_on(Color.PURPLE))
                 time.sleep(3)
-                TonePlayer(22).play(*['E5q',
-                                      'Be',
-                                      'C5e',
-                                      'D5e',
-                                      'D5e',
-                                      'C5e',
-                                      'Be'])
+                TonePlayer(22).play(*[
+                    'D5e',
+                    'C5e',
+                    'Be'
+                ])
                 break
 
         print('Done')
