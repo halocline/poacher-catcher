c0cbf94efffe67e2c3fe5ee4adefe9058bf91a16
Halocline <mdglissmann@gmail.com>
2020-02-23 12:36:18 -0700

    on off

--- src/poacher-catcher.py
+++ src/poacher-catcher.py
@@ -14,11 +14,14 @@ from aiy.leds import (Leds, Pattern, PrivacyLed, RgbLeds, Color)
 def main():
     print('Press Button to get started. Ctrl-C to quit.')
 
+    count = 0
+
     with Board() as board:
         while True:
             board.button.wait_for_press()
             print('ON')
             board.led.state = Led.ON
+            print(count)
             board.button.wait_for_release()
             print('OFF')
             board.led.state = Led.OFF
